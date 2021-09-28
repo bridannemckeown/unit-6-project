5bb74cf700d26aa67ec7a2456e142f13fbc77dfa
bridannemckeown <83795357+bridannemckeown@users.noreply.github.com>
2021-09-28 14:26:25 -0700

    added getRandomPhraseAsArray function

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ const startButton = document.querySelector('.btn__reset');
 console.log(startButton);
 let missed = 0;
 
+const phrases = [
+    'time of your life',
+    'whale of a time',
+    'over the moon',
+    'jumping with joy',
+    'on top of the world'
+];
+
 startButton.addEventListener('click', () => {
     const overlay = startButton.parentNode;
     overlay.style.display = 'none';
 });
 
-const phrases = [
-    'Time of your life',
-    'Whale of a time',
-    'Over the moon',
-    'Jumping with joy',
-    'On top of the world'
-];
+//return a random phrase from the phrases array
+const getRandomPhraseAsArray = (arr) => {
+ const randomNumber = Math.floor(Math.random() * arr.length);
+ return arr[randomNumber];
+}
+
+getRandomPhraseAsArray(phrases);
+
+const addPhraseToDisplay = () => {
+    
+}
