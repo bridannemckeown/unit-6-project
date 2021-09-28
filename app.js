13917b911aa858f8e786b85ef657391a84e4defc
bridannemckeown <83795357+bridannemckeown@users.noreply.github.com>
2021-09-28 13:57:06 -0700

    adding event listener for start button and phrases array plus testing

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const keyBoard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+const startButton = document.querySelector('.btn__reset');
+console.log(startButton);
 let missed = 0;
 
-console.log(phrase);
+startButton.addEventListener('click', () => {
+    const overlay = startButton.parentNode;
+    overlay.style.display = 'none';
+});
+
+const phrases = [
+    'Time of your life',
+    'Whale of a time',
+    'Over the moon',
+    'Jumping with joy',
+    'On top of the world'
+];
