5f32ca8a113773e89f19468fe5c71e862e3b1deb
bridannemckeown <83795357+bridannemckeown@users.noreply.github.com>
2021-09-28 15:03:44 -0700

    added and tested addPhraseToDisplay function

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const keyBoard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const startButton = document.querySelector('.btn__reset');
-console.log(startButton);
 let missed = 0;
 
 const phrases = [
@@ -20,11 +19,25 @@ startButton.addEventListener('click', () => {
 //return a random phrase from the phrases array
 const getRandomPhraseAsArray = (arr) => {
  const randomNumber = Math.floor(Math.random() * arr.length);
- return arr[randomNumber];
+ return arr[randomNumber].split("");
 }
 
-getRandomPhraseAsArray(phrases);
+const randomPhrase = getRandomPhraseAsArray(phrases);
 
-const addPhraseToDisplay = () => {
-    
+//loop that adds the random phrase selected to display
+const addPhraseToDisplay = (randomPhraseSelected) => {
+    const ul = phrase.firstElementChild;
+    for (let i=0; i < randomPhraseSelected.length ;i++) {
+        const listItem = document.createElement('li');
+        listItem.textContent = randomPhraseSelected[i];
+        ul.appendChild(listItem);
+        if ( randomPhraseSelected[i] !== " " ) {
+            listItem.className = "letter";
+        } else {
+            listItem.className = "space";
+        }
+    }
+    console.log(ul);   
 }
+
+addPhraseToDisplay(randomPhrase);
